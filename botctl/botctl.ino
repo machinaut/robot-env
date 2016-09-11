c3126c728f0de33eb4f76a8bcae188327ef31cc0
Alex Ray <a@machinaut.com>
2016-09-11 15:29:43 -0700

    simple arduino program, prints inputs

---
+++ botctl/botctl.ino
@@ -0,0 +1,67 @@
+// botctl - simple robot arm control
+#include <Servo.h>
+
+Servo baseSrv;  // Base Servo - Digital I/O 3
+Servo shoulderSrv;  // Shoulder Servo - Digital I/O 5
+Servo elbowSrv;  // Elbow Servo - Digital I/O 6
+Servo wristSrv;  // Wrist Servo - Digital I/O 9
+Servo gripSrv;  // Grip Servo - Digital I/O 10
+
+int basePot = 0;  // Base Joystick (potentiometer) - Analog I/O 0
+int shoulderPot = 1;  // Shoulder Joystick (potentiometer) - Analog I/O 1
+int elbowPot = 2;  // Elbow Joystick (potentiometer) - Analog I/O 2
+int wristPot = 3;  // Wrist Joystick (potentiometer) - Analog I/O 3
+int gripPot = 4;  // Grip Dial (potentiometer) - Analog I/O 4
+
+int captPin = 2;  // capture button - Digital I/O 2
+int playPin = 4;  // playback button - Digital I/O 4
+
+void printInputs(void);
+void centerServos(void);
+
+void setup() {
+  Serial.begin(115200);
+
+  baseSrv.attach(3);
+  shoulderSrv.attach(5);
+  elbowSrv.attach(6);
+  wristSrv.attach(9);
+  gripSrv.attach(10);
+  pinMode(captPin, INPUT);
+  pinMode(playPin, INPUT);
+
+  while (!Serial);  // Wait for serial port to connect
+}
+
+
+void loop() {
+  centerServos();
+  printInputs();
+}
+
+void printInputs(void) {
+  int basePos = analogRead(basePot);  // Base Joystick Position
+  int shoulderPos = analogRead(shoulderPot);  // Shoulder Joystick Position
+  int elbowPos = analogRead(elbowPot);  // Elbow Joystick Position
+  int wristPos = analogRead(wristPot);  // Wrist Joystick Position
+  int gripPos = analogRead(gripPot);  // Grip Joystick Position
+  int captVal = digitalRead(captPin);  // Capture Button Value (HIGH / LOW)
+  int playVal = digitalRead(playPin);  // Playback Button Value (HIGH / LOW)
+
+  Serial.print(basePos); Serial.print(',');
+  Serial.print(shoulderPos); Serial.print(',');
+  Serial.print(elbowPos); Serial.print(',');
+  Serial.print(wristPos); Serial.print(',');
+  Serial.print(gripPos); Serial.print(',');
+  Serial.print(captVal); Serial.print(',');
+  Serial.print(playVal); Serial.print('\n');
+}
+
+void centerServos(void) {
+  baseSrv.write(90);
+  shoulderSrv.write(90);
+  elbowSrv.write(90);
+  wristSrv.write(90);
+  gripSrv.write(90);
+}
+
