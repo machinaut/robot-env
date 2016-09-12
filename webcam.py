865f974e781714082d03f7efb0b4cd0c1728b09c
Alex Ray <a@machinaut.com>
2016-09-11 18:06:13 -0700

    Use moments to find "center of object"

--- webcam.py
+++ webcam.py
@@ -2,13 +2,24 @@
 import numpy as np
 import cv2
 
-blursize = 55
+blursize = 21
 
 vc = cv2.VideoCapture(1)
 
 # Orange?  These are HSV values.
 color_min = np.array([7,50,50], np.uint8)
 color_max = np.array([12,255,255], np.uint8)
+
+# detector
+params = cv2.SimpleBlobDetector_Params()
+params.filterByColor = False
+params.filterByConvexity = False
+params.filterByCircularity = False
+params.filterByInertia = False
+params.filterByArea = True
+params.minArea = 100
+params.maxArea = 10000
+detector = cv2.SimpleBlobDetector(params)
 
 while True:
   _, im = vc.read()
@@ -19,6 +30,12 @@ while True:
   hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
   # Threshold based on given min/max - restrictive in hue but generous in saturation/value
   thresh = cv2.inRange(hsv, color_min, color_max)
+  # Gaussian again for good measure
+  cv2.GaussianBlur(thresh, (blursize, blursize), 0)
   cv2.imshow('yo', thresh)
+  M = cv2.moments(thresh)
+  cx = int(M['m10']/M['m00'])
+  cy = int(M['m01']/M['m00'])
+  print('moments',cx,cy)
 
   cv2.waitKey(1)
