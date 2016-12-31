731ee7231effb4712afac5d5c43a846b96a9e5e4
Michael Bella <mjbella@sbcglobal.net>
2016-12-31 15:25:13 -0800

    Improved chessboard recognition

--- webcamcal.py
+++ webcamcal.py
@@ -2,6 +2,8 @@
 import sys
 import numpy as np
 import cv2
+import datetime
+import time
 
 def nothing(x):
     print('got x', x)
@@ -18,7 +20,7 @@ cap = cv2.VideoCapture(dev)
 cv2.namedWindow('frame')
 
 ######## Setup everything ahead of time for our calibration ########
-bsize = (5,4)
+bsize = (6,9)
 objpsz = bsize[0]*bsize[1]
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -31,31 +33,45 @@ objp[:,:2] = np.mgrid[0:bsize[0],0:bsize[1]].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-
+dtstart = datetime.datetime.today().strftime('%Y-%m-%d-%H%M%S')
+i = 0
+enable = True
+nextcap = time.time()
 while(True):
     # Capture frame-by-frame
     _, frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(frame, (5,5), 0)
+    #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, bsize,
-            cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE)
+    #opts =  cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE | cv2.CALIB_CB_FAST_CHECK  
+    opts = cv2.CALIB_CB_FAST_CHECK  
+    ret, corners = cv2.findChessboardCorners(gray, bsize, opts)
 
     # If found, add object points, image points (after refining them)
-    if ret == True:
+    if (ret == True) and (enable == True):
         print "FOUND!!"
         print corners
         print "\n"
         objpoints.append(objp)
 
+        cv2.imwrite("{}_found_board_{}.png".format(dtstart,i), gray)
+        i += 1
+        
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         imgpoints.append(corners2)
 
         # Draw and display the corners
         img = cv2.drawChessboardCorners(frame, bsize, corners2,ret)
+        enable = False
+        nextcap = time.time() + 2
     else:
         img = frame
  
+    if time.time() >= nextcap:
+        enable = True
+
     # Display the resulting frame
     cv2.imshow('frame', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -73,6 +89,9 @@ else:
     print 'dist', dist
     print 'rvecs', rvecs
     print 'tvecs', tvecs
+    datetime = datetime.datetime.today().strftime('%Y-%m-%d-%H%M%S')
+    np.savez('./camera_cal_{}'.format(datetime), ret=ret, mtx=mtx, dist=dist,
+            rvecs=rvecs, tvecs=tvecs)
 
 # When everything done, release the capture
 cap.release()
